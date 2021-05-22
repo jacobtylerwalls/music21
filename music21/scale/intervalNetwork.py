3e214a04a6b8c761f41406f6d74579848f10692c
Jacob Walls <jacobtylerwalls@gmail.com>
2021-05-22 16:49:25 -0400

    Fix typo in class name

--- music21/scale/intervalNetwork.py
+++ music21/scale/intervalNetwork.py
@@ -2254,7 +2254,7 @@ class IntervalNetwork:
         # import is here to avoid import of matplotlib problems
         from music21 import graph
         # first ordered arg can be method type
-        g = graph.primitives.GraphNetworxGraph(
+        g = graph.primitives.GraphNetworkxGraph(
             networkxGraph=self.getNetworkxGraph())
 
         # networkxGraph=self.getNetworkxRealizedGraph(pitchObj=pitchObj,
